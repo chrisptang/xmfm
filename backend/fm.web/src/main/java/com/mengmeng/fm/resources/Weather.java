e40fa55c96171bf3d2d7f805c7ed1733a9f8a9c3
derrick.tangp <chris.p.tang@gmail.com>
2014-05-25 16:28:48 +0800

    add packages

---
+++ backend/fm.web/src/main/java/com/mengmeng/fm/resources/Weather.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the confidential and proprietary information of
+ * Alibaba.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with Alibaba.com.
+ */
+package com.mengmeng.fm.resources;
+
+/**
+ * @author derrick.tangp May 25, 2014 4:25:16 PM
+ */
+public class Weather {
+
+}
