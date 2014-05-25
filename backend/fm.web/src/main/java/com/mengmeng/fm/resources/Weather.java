4b29991e7b24c105d8e054747e5b89f093d3755d
derrick.tangp <chris.p.tang@gmail.com>
2014-05-25 17:00:25 +0800

    xcdsc

--- backend/fm.web/src/main/java/com/mengmeng/fm/resources/Weather.java
+++ backend/fm.web/src/main/java/com/mengmeng/fm/resources/Weather.java
@@ -5,9 +5,38 @@
  */
 package com.mengmeng.fm.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
 /**
  * @author derrick.tangp May 25, 2014 4:25:16 PM
  */
+@Component
+@Path("/weather")
+@Scope("request")
 public class Weather {
 
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+    @GET
+    @Path("/getCityList")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCityList() {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
 }
