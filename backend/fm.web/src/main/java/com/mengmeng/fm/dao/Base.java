9d2046419098cd6093f9a2c2502701bf769b315e
derrick.tangp <chris.p.tang@gmail.com>
2014-05-25 17:10:04 +0800

    adding packages

---
+++ backend/fm.web/src/main/java/com/mengmeng/fm/dao/Base.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package com.mengmeng.fm.dao;
+
+/**
+ * 类Base.java的实现描述：TODO 类实现描述 
+ * @author derrick.tangp May 25, 2014 5:08:15 PM
+ */
+public class Base {
+
+}
---
+++ backend/fm.web/src/main/java/com/mengmeng/fm/filter/AudiFilter.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 1999-2004 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package com.mengmeng.fm.filter;
+
+/**
+ * 类AudiFilter.java的实现描述：TODO 类实现描述 
+ * @author derrick.tangp May 25, 2014 5:07:26 PM
+ */
+public class AudiFilter {
+
+}
