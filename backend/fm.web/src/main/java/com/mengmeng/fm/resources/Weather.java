b7aa03d94232304acafbfd6ab1177d41fbab0835
PENG TANG <pengtang@PENGs-MacBook-Pro.local>
2018-10-24 15:53:49 +0800

    upgrading spring version

--- backend/fm.web/src/main/java/com/mengmeng/fm/resources/Weather.java
+++ backend/fm.web/src/main/java/com/mengmeng/fm/resources/Weather.java
@@ -5,9 +5,13 @@
  */
 package com.mengmeng.fm.resources;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
@@ -34,7 +38,7 @@ public class Weather {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getCityList() {
 
-        String output = "Jersey say : " + msg;
+        String output = "Jersey say : ";
 
         return Response.status(200).entity(output).build();
 
